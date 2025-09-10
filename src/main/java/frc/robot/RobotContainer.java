1ed62220587f1fd610398a57d22eeaf045717881
Ninjago77 <shanvantharunmozhi@gmail.com>
2025-09-10 15:54:24 -0700

    first

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,55 +4,18 @@
 
 package frc.robot;
 
-import frc.robot.constants.ElevatorConstants;
 import frc.robot.constants.OperatorConstants;
 import frc.robot.commands.AutoBack;
-import frc.robot.commands.Autos;
-import frc.robot.commands.CageClimberClimb;
-import frc.robot.commands.CageClimberDrop;
-import frc.robot.commands.DealgerDown;
-import frc.robot.commands.DealgerDrop;
-import frc.robot.commands.DealgerUp;
-import frc.robot.commands.ElevatorManualUp;
-import frc.robot.commands.ElevatorManualDown;
-import frc.robot.commands.ElevatorTestOFF;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.HexAlign;
-import frc.robot.commands.ElevatorTestDOWN;
-import frc.robot.commands.ElevatorTestUP;
-import frc.robot.commands.ElevatorTestOFF;
-import frc.robot.subsystems.CageClimber;
-import frc.robot.subsystems.Dealger;
-import frc.robot.subsystems.Elevator;
-import frc.robot.subsystems.ElevatorTestSubsystem;
-
-
-import frc.robot.commands.EndEffectorDrop;
-import frc.robot.commands.EndEffectorGrab;
-import frc.robot.commands.EndEffectorReverse;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.HexAlign;
-import frc.robot.commands.TriggerTest;
-
-import frc.robot.subsystems.EndEffector;
-
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.RgbLEDs;
 import frc.robot.subsystems.SwerveDrive;
-import frc.robot.subsystems.climber;
 import frc.lib.Controller;
 import frc.lib.FlightControl;
 
-import java.util.PrimitiveIterator;
-
-import com.pathplanner.lib.commands.PathPlannerAuto;
-
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -68,23 +31,10 @@ public class RobotContainer {
 
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   //private final ElevatorTestSubsystem m_ElevatorTestSubsystem = new ElevatorTestSubsystem();
-  private final Elevator elevator = new Elevator();
   private final SwerveDrive sd = new SwerveDrive();
 
   private final Controller controller = new Controller(1 );
   private final Controller teoController = new Controller(2);
-  private final EndEffector endEffector = new EndEffector();
-  private final Dealger dealger = new Dealger();
-  private final EndEffectorGrab endEffectorGrab = new EndEffectorGrab(endEffector);
-  private final EndEffectorDrop endEffectorDrop = new EndEffectorDrop(endEffector);
-  private final EndEffectorReverse endEffectorReverse = new EndEffectorReverse(endEffector);
-  private final DealgerDown dealgerdown = new DealgerDown(dealger);
-  private final DealgerUp dealgerup = new DealgerUp(dealger);
-  private final DealgerDrop dealgerDrop = new DealgerDrop(dealger);
-
-
-  private final ElevatorManualUp manualUp = new ElevatorManualUp(elevator);
-  private final ElevatorManualDown manualDown = new ElevatorManualDown(elevator);
 
   private final Limelight cam =  new Limelight();
   private final FlightControl flightcont = new FlightControl(3);
@@ -100,10 +50,7 @@ public class RobotContainer {
   //private final ElevatorTestUP elevatorTestUP = new ElevatorTestUP(m_ElevatorTestSubsystem);
   //private final ElevatorTestDOWN elevatorTestDOWN = new ElevatorTestDOWN(m_ElevatorTestSubsystem);
 
-  private final CageClimber cageClimber = new CageClimber();
-  private final CageClimberClimb climb = new CageClimberClimb(cageClimber);
-  private final CageClimberDrop drop = new CageClimberDrop(cageClimber);
-  private final RgbLEDs rgbLeds = new RgbLEDs();
+  // private final RgbLEDs rgbLeds = new RgbLEDs();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -215,57 +162,11 @@ public class RobotContainer {
   public void configureOperatorBindings(){
    
     
-    //climber commands
-    controller.getLeftBumper().whileTrue(climb);
-    controller.getRightBumper().whileTrue(drop);
     
     //elevator commands
     /*controller.getY().whileTrue(elevatorTestUP);
     controller.getX().whileTrue(elevatorTestDOWN);
     controller.getB().whileTrue(elevatorTestOFF);*/
-    
-    controller.getBack().onTrue(
-      new InstantCommand(
-        () -> elevator.toggleAlgaeDown()
-      )
-    );
-
-    controller.getStart().onTrue(
-      new InstantCommand(
-        () -> elevator.toggleAlgaeUp()
-      )
-    );
-
-    
-    controller.getY().onTrue(
-      new InstantCommand(
-        () -> elevator.toggleHeightUp()
-      )
-    );
-
-    controller.getA().onTrue(
-      new InstantCommand(
-        () -> elevator.toggleHeightDown()
-      )
-    );
-    
-
-    
-    
-    controller.getLeftStick().whileTrue(manualUp);
-    controller.getRightStick().whileTrue(manualDown);
-
-    //controller.getA().whileTrue(dealgerdown);
-
-    //end effector commands
-    controller.getLeftTrigger().whileTrue(endEffectorGrab);
-    controller.getRightTrigger().whileTrue(endEffectorDrop);
-    controller.getLeft().whileTrue(endEffectorReverse);
-
-
-    controller.getUp().whileTrue(dealgerup);
-    controller.getDown().whileTrue(dealgerdown);
-    controller.getRight().whileTrue(dealgerDrop);
     
 
   }
